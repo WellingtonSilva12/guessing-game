64e6c54c25cec489eb05a40c3f5c7678e2b52d44
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2022-09-15 17:25:19 -0300

    study on functions and refactoring

---
+++ script.js
@@ -0,0 +1,43 @@
+const inputNumber = document.querySelector('.input-text')
+const formSubmit = document.querySelector('form')
+const hideScreen1 = document.querySelector('.screen1')
+const showScreen2 = document.querySelector('.screen2')
+const btnPlayAgain = document.querySelector('.btn-again')
+
+let randomNumber = randomNumberGenerator()
+let xAttempts = 1
+
+formSubmit.addEventListener('submit', submitForm)
+btnPlayAgain.addEventListener('click', handlePlayAgain)
+
+function submitForm(e) {
+  e.preventDefault()
+
+  if (Number(inputNumber.value) == randomNumber) {
+    toogleScreen()
+
+    showScreen2.querySelector(
+      'h2'
+    ).innerText = `Acertou em ${xAttempts} tentativas`
+  }
+  console.log(xAttempts, randomNumber)
+
+  inputNumber.value = ''
+
+  xAttempts++
+}
+function handlePlayAgain() {
+  toogleScreen()
+  randomNumber = Math.round(Math.random() * 10)
+
+  console.log(randomNumber)
+  xAttempts = 1
+}
+function toogleScreen() {
+  hideScreen1.classList.toggle('hide')
+  showScreen2.classList.toggle('hide')
+}
+function randomNumberGenerator() {
+  let result = Math.round(Math.random() * 10)
+  return result
+}
